d361593630ed74b164cb21cf6b3b1f12c0cd7b3f
Chris Iverson <cj.no.one@gmail.com>
2018-10-19 16:08:45 -0500

    Initial Import

---
+++ AGSOutOfOfficeService/Service1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace AGSOutOfOfficeService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private EventLog eventLog1;
+
+        public Service1()
+        {
+            InitializeComponent();
+            eventLog1 = EventLog;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("AGSOutOfOfficeService Start");
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("AGSOutOfOfficeService Stop");
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            eventLog1.WriteEntry("AGSOooSrv Background Worker");
+            while(!backgroundWorker1.CancellationPending)
+            {
+                Thread.Sleep(5000);
+            }
+
+            eventLog1.WriteEntry("AGSOooSrv Background Worker Cancel");
+            e.Cancel = true;
+        }
+    }
+}
